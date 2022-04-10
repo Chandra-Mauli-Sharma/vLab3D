ecd9627ad5b181c725c7c67b617496c42fc2b067
chandramaulisharma <chandramaulisharma26@gmail.com>
2022-04-10 23:19:58 +0530

    Minor UI Tweaks

---
+++ Assets/ElectricalMenuManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ElectricalMenuManager : MonoBehaviour
+{
+    public void Resistor(){
+        SceneManager.LoadScene("Familiarization Resistor");
+    }
+}
