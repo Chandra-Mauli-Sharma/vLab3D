051bec40c07aa319615dcb05c3cd77acbd73cee0
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2022-08-11 22:19:40 +0530

    Stack Modification

--- Assets/StackElement.cs
+++ Assets/StackElement.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace StackStimulator
 {
     public class StackElement: MonoBehaviour
     {
-        public float value;
+        public string value;
         public GameObject element;
 
         void Start(){
             element=gameObject;
         }
-        public StackElement(float value,GameObject element)
+        public StackElement(string value,GameObject element)
         {
             this.value = value;
             this.element=element;
+            element.GetComponentInChildren<TMP_Text>().text=value.ToString();
         }
     }
 }
--- Assets/StackStimulation.cs
+++ Assets/StackStimulation.cs
@@ -2,37 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StackStimulator;
-using UnityEditor.Scripting.Python;
 using System.IO;
+using TMPro;
 
 public class StackStimulation : MonoBehaviour
 {
     public GameObject stackElement;
+    public TMP_InputField value;
     Stack<StackElement> stack = new Stack<StackElement>();
     public GameObject posTracker;
-    int i;
-    // Start is called before the first frame update
-    void Start()
-    {
-        i = 0;
-        RunEnsureNaming();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    static void RunEnsureNaming()
-    {
-        string scriptPath = Path.Combine(Application.dataPath, "ex.py");
-        PythonRunner.RunFile(scriptPath);
-    }
 
     public void PushElement()
     {
-        stack.Push(new StackElement(posTracker.transform.position.y, Instantiate(stackElement, posTracker.transform.position, Quaternion.identity)));
+        stack.Push(new StackElement(value.text, Instantiate(stackElement, posTracker.transform.position, Quaternion.identity)));
         posTracker.transform.position += new Vector3(0, 0.5f, 0);
     }
 
